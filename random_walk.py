d285b261e8b99219278008e2440747a239adab98
Madeleine <m.monica6889@gmail.com>
2020-08-22 20:32:19 -0400

    molecular motion

--- random_walk.py
+++ random_walk.py
@@ -36,3 +36,4 @@ class RandomWalk:
 
 			self.x_values.append(x)
 			self.y_values.append(y)
+			
--- rw_visual.py
+++ rw_visual.py
@@ -2,12 +2,28 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk 
 
-# make a random walk
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks,as long as the program is active.
+while True:
+	# Make a random walk
+	rw = RandomWalk()
+	rw.fill_walk()
 
-# Plot the points in the walk 
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s = 15)
-plt.show()
+	# Plot the points in the walk 
+	plt.style.use('classic')
+	fig, ax = plt.subplots(figsize = (15,9))
+	point_numbers = range(rw.num_points)
+	ax.plot(rw.x_values, rw.y_values, linewidth = 0.5, zorder = 1)
+	# Emphasize the first and the last points.
+	ax.scatter(0, 0, c = 'red', edgecolors = 'black', s = 200, zorder = 2)
+	ax.scatter(rw.x_values[-1], rw.y_values[-1], c = 'yellow', edgecolors = 'black',
+		s = 200, zorder = 2)
+
+	# Remove the axis.
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
+
+
+	plt.show()
+	keep_running = input("make another walk? (y/n): ")
+	if keep_running == n:
+		break
